7abc8bf312c55cce6c7e42a7545a24838b3eb80b
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2022-11-03 11:31:31 +0100

    success and fail messages

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,50 +1,85 @@
-import React, { useState } from 'react'
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
 
 function Signup() {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
 
-    const [userData, setUserData] = useState({
-        email:"",
-        password:"",
-        name:""
-    })
+  const [message, setMessage] = useState(false);
+  const [failMessage, setFailMessage] = useState(false);
 
-
-
-    const submitHandler = (e) =>{
-        e.preventDefault()
-        setUserData({
-            name:e.target.name.value,
-            email:e.target.email.value,
-            password:e.target.password.value
-        })
-        axios.post(`${process.env.REACT_APP_BE_URL}/auth/signup`,userData)
-    .then(res=>console.log("response from backend", res))
-    .catch(err => console.log(err))
-
-    }
-    return (
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    });
+    axios
+      .post(`${process.env.REACT_APP_BE_URL}/auth/signup`, userData)
+      .then((res) => {
+        setFailMessage(false);
+        setMessage(true);
+      })
+      .catch((err) => {
+        if (err.response.data.status === "failed") {
+          setMessage(false);
+          setFailMessage(true);
+        }
+      });
+  };
+  return (
+    <div>
+      <h3>Register</h3>
+      <form action="post" onSubmit={submitHandler}>
         <div>
-            <h3>Register</h3>
-            <form action='post' onSubmit={submitHandler}>
-                <div>
-                <div>
-                <label htmlFor="name">Name   :  </label>
-                <input type="text" id="name" placeholder="enter your name" name="name" />
-                </div>
-                <label htmlFor="email">Email   :  </label>
-                <input type="email" id="email" placeholder="enter your email" name="email"/>
-                </div>
-
-                <div>
-                <label htmlFor="password">password   :  </label>
-                <input type="password" id="password" placeholder="enter your password" name="password" />
-                </div>
-
-                <button type='submit'> Signup</button>
-            </form>
+          <div>
+            <label htmlFor="name">Name : </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="enter your name"
+              name="name"
+            />
+          </div>
+          <label htmlFor="email">Email : </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="enter your email"
+            name="email"
+          />
         </div>
-    )
+
+        <div>
+          <label htmlFor="password">password : </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="enter your password"
+            name="password"
+          />
+        </div>
+
+        <button type="submit"> Signup</button>
+      </form>
+      <hr />
+      {message ? (
+        <h3 style={{ color: "green" }}>Data inserted successfully!</h3>
+      ) : (
+        ""
+      )}
+
+      {failMessage ? (
+        <h3 style={{ color: "red" }}>User already exists!!</h3>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
