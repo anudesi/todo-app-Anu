0bb9881806aaba112a92a995f5875d26b63145cf
atherahmad <atherahmad49@yahoo.com>
2022-11-10 22:48:50 +0100

    signin and verify token

--- client/src/components/Auth/Signin.jsx
+++ client/src/components/Auth/Signin.jsx
@@ -1,66 +1,67 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from "axios"
 import { useNavigate } from 'react-router-dom'
 
 function Signin(props) {
 
     const [userData, setUserData] = useState({
-        email:"",
-        password:"",
-  
+        email: "",
+        password: "",
+
     })
 
-const {itCouldBeAnyName} = props
+    const { itCouldBeAnyName } = props
 
-const navigate = useNavigate()
-const [error, setError] = useState("")
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
 
-const blurHandler = (e) =>{
-    
+    const blurHandler = (e) => {
+
         const propertyName = e.target.name
         const propertyValue = e.target.value
-        setUserData(oldState=>{
+        setUserData(oldState => {
             return {
                 ...oldState,
-        [propertyName]: propertyValue,
-        
-    }}
-    )
+                [propertyName]: propertyValue,
 
-}
+            }
+        }
+        )
+
+    }
 
 
-    const submitHandler = (e) =>{
+    const submitHandler = (e) => {
         e.preventDefault()
 
-        axios.post(`${process.env.REACT_APP_BE_URL}/auth/signin`,userData)
-    .then(res=>{
-        localStorage.setItem("toDoToken",JSON.stringify(res.data.data.token))
-        itCouldBeAnyName()
-        navigate("/dashboard")
-    })
-    .catch(err => setError(err.response.data.message))
-    }    
+        axios.post(`${process.env.REACT_APP_BE_URL}/auth/signin`, userData)
+            .then(res => {
+                localStorage.setItem("toDoToken", JSON.stringify(res.data.data.token))
+                itCouldBeAnyName()
+                navigate("/dashboard")
+            })
+            .catch(err => setError(err.response.data.message))
+    }
     return (
-    <div>
-        <h3>Log In</h3>
-        <form action='post' onSubmit={submitHandler}>
-            <div>
-            <label htmlFor="email">Email   :  </label>
-            <input type="email" id="email" placeholder="enter your email" name="email" autoComplete='none' onBlur={blurHandler}/>
-            </div>
+        <div>
+            <h3>Log In</h3>
+            <form action='post' onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="email">Email   :  </label>
+                    <input type="email" id="email" placeholder="enter your email" name="email" autoComplete='none' onBlur={blurHandler} />
+                </div>
 
-            <div>
-            <label htmlFor="password">password   :  </label>
-            <input type="password" id="password" placeholder="enter your password" name="password" onBlur={blurHandler}/>
-            </div>
+                <div>
+                    <label htmlFor="password">password   :  </label>
+                    <input type="password" id="password" placeholder="enter your password" name="password" onBlur={blurHandler} />
+                </div>
 
-            <button type='submit'> Log In</button>
-            <hr/>
-            {
-                error? (<p>{error}</p>) : null
-            }
-        </form>
+                <button type='submit'> Log In</button>
+                <hr />
+                {
+                    error ? (<p>{error}</p>) : null
+                }
+            </form>
 
         </div>
     )
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,11 +2,13 @@
 import jwt from "jsonwebtoken"
 
 export const verifyToken = (req, res, next) =>{
+
     try{
     
     const token = req.headers.authorization?.split(" ")[1]
     if(!token) return  res.status(401).json({message: "Not Authorized"})
     const authorized = jwt.verify(token,process.env.JWT_SECRET )
+    console.log(authorized)
     if(authorized)
     // We will call next only when if it is a valid
     next()
