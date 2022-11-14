ac7aad70967a274de2e9b75132f3fea819f5ed2c
anu <anupama.desikan@gmail.com>
2022-11-14 15:13:58 +0100

    update

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname,"./client", "build", "index.html"));
 });
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
   
     app.listen(PORT, () => {
